8e95204574ba12b59714723925cb221651db9e36
Michael <mbrockman1@gmail.com>
2026-04-27 21:24:01 -0400

    Initial Commit

---
+++ sip-dynamic-hydration/sip_dynamic_hydrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  sip_dynamic_hydrationApp.swift
+//  sip-dynamic-hydration
+//
+//  Created by Michael Brockman on 4/27/26.
+//
+
+import SwiftUI
+
+@main
+struct sip_dynamic_hydrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
